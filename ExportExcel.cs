3b011602b50a44efec77f38c55282e91cefe63f8
dirk.roessger@gmail.com <Cadplayer123>
2018-05-04 19:23:20 +0200

    Revert "WIP on master: 15031d7 Acad_table"
    This reverts commit 390ccf2fec559ce53b71fda1b54593437ea1c126, reversing
    changes made to 15031d7eccc672dda9ead53472208723fa6b5ef7.
    
    # Conflicts:
    #	.vs/C3D_table_export/v14/.suo

---
+++ ExportExcel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace C3D_table_export
+{
+    public class ExportExcel
+    {
+        public static void ExportToExcel(List<TableData> list)
+        {
+
+            Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+
+            if (xlApp == null)
+            {
+                MessageBox.Show("Excel is not properly installed!!");
+                return;
+            }
+
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+            object misValue = System.Reflection.Missing.Value;
+
+            xlWorkBook = xlApp.Workbooks.Add(misValue);
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                int row = int.Parse(list[i].Line);
+                List<string> values = list[i].Values;
+                int index = 0;
+
+                foreach (string x in values)
+                {
+                    index++;
+                    xlWorkSheet.Cells[row + 1, index] = values[index - 1];
+                }
+            }
+            //xlWorkSheet.Cells[1, 1] = list[0].Values[0];
+            //xlWorkSheet.Cells[1, 2] = list[0].Values[1];
+            //xlWorkSheet.Cells[1, 3] = list[0].Values[2];
+
+            xlWorkBook.SaveAs("c:\\temp\\csharp-Excel.xls", Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+            xlWorkBook.Close(true, misValue, misValue);
+            xlApp.Quit();
+
+
+            MessageBox.Show("Excel file created , you can find the file csharp-Excel.xls");
+
+        }
+    }
+}
+
+
---
+++ ExternDwg.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+
+namespace C3D_table_export
+{
+    public class ExternDwg
+    {
+        public static void CopySpaceToExtDWG()
+        {
+            // get the working database (in AutoCAD)
+            Database sourceDb = Application.DocumentManager.MdiActiveDocument.Database;
+
+            try
+            {
+                // create a new destination database
+                using (Database destDb = new Database(true, true))
+                {
+                    // get the model space object ids for both dbs
+                    ObjectId sourceMsId = SymbolUtilityServices.GetBlockModelSpaceId(sourceDb);
+                    ObjectId destDbMsId = SymbolUtilityServices.GetBlockModelSpaceId(destDb);
+
+                    // now create an array of object ids to hold the source objects to copy
+                    ObjectIdCollection sourceIds = new ObjectIdCollection();
+
+                    // open the sourceDb ModelSpace (current autocad dwg)
+                    using (BlockTableRecord ms =  sourceMsId.Open(OpenMode.ForRead) as BlockTableRecord)
+
+                        // loop all the entities and record their ids
+                        foreach (ObjectId id in ms)
+                            sourceIds.Add(id);
+
+                    // next prepare to deepclone the recorded ids to the destdb
+                    IdMapping mapping = new IdMapping();
+
+                    // now clone the objects into the destdb
+                    sourceDb.WblockCloneObjects(sourceIds, destDbMsId, mapping, DuplicateRecordCloning.Replace, false);
+
+                    destDb.SaveAs("c:\\temp\\CopyTest.dwg", DwgVersion.Current);
+                }
+
+            }
+
+            catch (System.Exception eXP)
+
+            {
+
+                System.Windows.Forms.MessageBox.Show(eXP.ToString());
+
+            }
+        }
+    }
+}
